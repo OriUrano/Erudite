79341faffbbba9f3022ce430e76915c420958b06
Ori <77636823+OriUrano@users.noreply.github.com>
2024-09-16 16:37:36 +0000

    bruh

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 
 genai.configure(api_key=os.environ["GEMINI"])
 
-sample_file = genai.upload_file(path="nosesoff.pdf",
-                                display_name="Noses Off Script")
+sample_file = genai.upload_file(path="AMSCO+1.6.pdf",
+                                display_name="AMSCO 1.6")
 
 print(f"Uploaded file '{sample_file.display_name}' as: {sample_file.uri}")
 
@@ -13,9 +13,23 @@ file = genai.get_file(name=sample_file.name)
 print(f"Retrieved file '{file.display_name}' as: {sample_file.uri}")
 
 # Choose a Gemini model.
-model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest")
+
+prompt = "Rewrite this into Markdown word for word. Dont include page numbers, images, image caption, questions about connecting to other subjects"
 
 # Prompt the model with text and the previously uploaded image.
-response = model.generate_content([sample_file, "Can you summarize this document as a bulleted list?"])
+response = model.generate_content([sample_file, 
+    prompt])
 
 print(response.text)
+
+f = open("response.txt", "a")
+f.write("\nPrompt:\n")
+f.write(prompt)
+f.write("\nResponse:\n")
+f.write(response.text)
+f.close()
+
+for file in genai.list_files():
+    genai.delete_file(file.name)
+    print(f"{file.display_name}, URI: {file.uri}, Name: {file.name}")
